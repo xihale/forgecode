541eefc034244f120858bc533720bbe02b1ef0c3
xihale <xihale.top@qq.com>
2026-04-29 14:00:30 +0800

    Revert "fix: guard forge fish setup with command availability check"
    This reverts commit d7364417f555c556386b01a081c3ed32ade1916b.

--- shell-plugin/forge.setup.fish
+++ shell-plugin/forge.setup.fish
@@ -2,11 +2,11 @@
 # !! Do not edit manually - changes will be overwritten !!
 
 # Load forge shell plugin (commands, completions, keybindings) if not already loaded
-if command -q forge; and test -z "$_FORGE_PLUGIN_LOADED"
+if test -z "$_FORGE_PLUGIN_LOADED"
     forge fish plugin | source
 end
 
 # Load forge shell theme (prompt with AI context) if not already loaded
-if command -q forge; and test -z "$_FORGE_THEME_LOADED"
+if test -z "$_FORGE_THEME_LOADED"
     forge fish theme | source
 end
